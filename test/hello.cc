cefc689b42b2bccd302f71414f36903c926b8b7a
fadhil riyanto <id.fadhil.riyanto@gmail.com>
2024-07-07 00:18:58 +0700

    fix bug while use -DLOG_USE_COLOR

--- test/hello.cc
+++ test/hello.cc
@@ -1,5 +1,10 @@
 #include "../src/log.h"
 
 int main() {
+    log_trace("hello!!!");
     log_debug("hello!!!");
+    log_info("hello!!!");
+    log_warn("hello!!!");
+    log_error("hello!!!");
+    log_fatal("hello!!!");
 }
